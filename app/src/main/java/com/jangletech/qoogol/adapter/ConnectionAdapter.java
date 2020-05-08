8ae5750c8ae5c77c64f0421e25a68d8277f19b3e
Pritali <pritali.kuyate@jangletech.com>
2020-05-08 16:32:24 +0530

    connetion changes added i.e. friends/followers/followings/friend requests/blocked connections

---
+++ app/src/main/java/com/jangletech/qoogol/adapter/ConnectionAdapter.java
@@ -0,0 +1,260 @@
+package com.jangletech.qoogol.adapter;
+
+import android.app.Activity;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.FragmentActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.jangletech.qoogol.R;
+import com.jangletech.qoogol.databinding.ConnectionItemBinding;
+import com.jangletech.qoogol.dialog.ProgressDialog;
+import com.jangletech.qoogol.model.Connections;
+import com.jangletech.qoogol.model.ResponseObj;
+import com.jangletech.qoogol.retrofit.ApiClient;
+import com.jangletech.qoogol.retrofit.ApiInterface;
+import com.jangletech.qoogol.util.UtilHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+import static android.view.Gravity.END;
+import static com.jangletech.qoogol.ui.BaseFragment.getDeviceId;
+import static com.jangletech.qoogol.util.Constant.accept_follow_requests;
+import static com.jangletech.qoogol.util.Constant.accept_friend_requests;
+import static com.jangletech.qoogol.util.Constant.block;
+import static com.jangletech.qoogol.util.Constant.follow;
+import static com.jangletech.qoogol.util.Constant.followers;
+import static com.jangletech.qoogol.util.Constant.following;
+import static com.jangletech.qoogol.util.Constant.followrequests;
+import static com.jangletech.qoogol.util.Constant.friendrequests;
+import static com.jangletech.qoogol.util.Constant.friends;
+import static com.jangletech.qoogol.util.Constant.qoogol;
+import static com.jangletech.qoogol.util.Constant.reject_follow_requests;
+import static com.jangletech.qoogol.util.Constant.reject_friend_requests;
+import static com.jangletech.qoogol.util.Constant.remove_connection;
+import static com.jangletech.qoogol.util.Constant.unblock;
+import static com.jangletech.qoogol.util.Constant.unfollow;
+
+/**
+ * Created by Pritali on 5/6/2020.
+ */
+public class ConnectionAdapter extends RecyclerView.Adapter<ConnectionAdapter.ViewHolder> implements Filterable {
+    List<Connections> connectionsList;
+    Activity activity;
+    ConnectionItemBinding connectionItemBinding;
+    List<Connections> filteredConnectionsList;
+    String call_from;
+    updateConnectionListener listener;
+
+    public ConnectionAdapter(Activity activity, List<Connections> connectionsList, String call_from, updateConnectionListener listener) {
+        this.activity = activity;
+        this.connectionsList = connectionsList;
+        this.filteredConnectionsList = connectionsList;
+        this.call_from = call_from;
+        this.listener = listener;
+    }
+
+    @NonNull
+    @Override
+    public ConnectionAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        connectionItemBinding = DataBindingUtil.inflate(
+                LayoutInflater.from(parent.getContext()),
+                R.layout.connection_item, parent, false);
+        return new ViewHolder(connectionItemBinding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ConnectionAdapter.ViewHolder holder, int position) {
+        Connections connections = connectionsList.get(position);
+        connectionItemBinding.tvUserName.setText(connections.getU_first_name() + " " + connections.getU_last_name());
+        try {
+            Glide.with(activity).load(UtilHelper.getProfilePath(connections.getCn_user_id_2(),connections.getProf_pic().trim())).circleCrop().placeholder(R.drawable.profile).into(connectionItemBinding.userProfileImage);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+
+        PopupMenu popup = new PopupMenu(activity, connectionItemBinding.textViewOptions,END);
+        popup.setGravity(END);
+        popup.inflate(R.menu.connection_options);
+        Menu popupMenu = popup.getMenu();
+        if (call_from.equalsIgnoreCase(friends)) {
+            popupMenu.findItem(R.id.action_remove_connection).setVisible(true);
+            if (connections.getCn_blocked_by_u1().equalsIgnoreCase("false"))
+                popupMenu.findItem(R.id.action_follow).setVisible(true);
+        } else if (call_from.equalsIgnoreCase(followers)) {
+            popupMenu.findItem(R.id.action_follow).setVisible(true);
+            if (connections.getCn_connected().equalsIgnoreCase("false"))
+                popupMenu.findItem(R.id.action_add_friend).setVisible(true);
+        } else if (call_from.equalsIgnoreCase(following)) {
+            popupMenu.findItem(R.id.action_unfollow).setVisible(true);
+            if (connections.getCn_connected().equalsIgnoreCase("false"))
+                popupMenu.findItem(R.id.action_add_friend).setVisible(true);
+        } else if (call_from.equalsIgnoreCase(friendrequests)) {
+            try {
+                if (connections.getFriend_req_sent().equalsIgnoreCase("true")) {
+                    popupMenu.findItem(R.id.cancel_friend).setVisible(true);
+                } else {
+                    popupMenu.findItem(R.id.accept_friend).setVisible(true);
+                    popupMenu.findItem(R.id.reject_friend).setVisible(true);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+
+        } else if (call_from.equalsIgnoreCase(followrequests)) {
+            try {
+                if (connections.getFollow_req_sent().equalsIgnoreCase("true")) {
+                    popupMenu.findItem(R.id.cancel_follow).setVisible(true);
+                } else {
+                    popupMenu.findItem(R.id.accept_follow).setVisible(true);
+                    popupMenu.findItem(R.id.reject_follow).setVisible(true);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+        }
+
+
+        popup.setOnMenuItemClickListener(item -> {
+            switch (item.getItemId()) {
+                case R.id.action_remove_connection:
+                    updateConnection(connections.getCn_user_id_2(),remove_connection);
+                    break;
+                case R.id.action_block:
+                    updateConnection(connections.getCn_user_id_2(),block);
+                    break;
+                case R.id.action_unblock:
+                    updateConnection(connections.getCn_user_id_2(),unblock);
+                    break;
+                case R.id.action_follow:
+                    updateConnection(connections.getCn_user_id_2(),follow);
+                    break;
+                case R.id.action_unfollow:
+                    updateConnection(connections.getCn_user_id_2(),unfollow);
+                    break;
+                    case R.id.accept_follow:
+                    updateConnection(connections.getCn_user_id_2(),accept_follow_requests);
+                    break;
+                case R.id.reject_follow:
+                case R.id.cancel_follow:
+                    updateConnection(connections.getCn_user_id_2(),reject_follow_requests);
+                    break;
+                case R.id.accept_friend:
+                    updateConnection(connections.getCn_user_id_2(),accept_friend_requests);
+                    break;
+                case R.id.reject_friend:
+                case R.id.cancel_friend:
+                    updateConnection(connections.getCn_user_id_2(),reject_friend_requests);
+                    break;
+
+            }
+            return false;
+        });
+        connectionItemBinding.textViewOptions.setOnClickListener(v -> {
+            popup.show();
+        });
+
+        if (position == connectionsList.size() - 1 && connectionsList.size() >= 250) {
+            listener.onBottomReached(connectionsList.size());
+        }
+
+    }
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                String charString = charSequence.toString();
+                if (charString.isEmpty()) {
+                    connectionsList = filteredConnectionsList;
+                } else {
+                    List<Connections> filteredList = new ArrayList<>();
+                    for (Connections row : filteredConnectionsList) {
+                        if (row.getU_first_name().toLowerCase().contains(charString.toLowerCase()) || row.getU_last_name().toLowerCase().contains(charString.toLowerCase())) {
+                            filteredList.add(row);
+                        }
+                    }
+                    connectionsList = filteredList;
+                }
+
+                FilterResults filterResults = new FilterResults();
+                filterResults.values = connectionsList;
+                return filterResults;
+            }
+
+            @Override
+            protected void publishResults(CharSequence constraint, FilterResults filterResults) {
+                connectionsList = (ArrayList<Connections>) filterResults.values;
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+    public interface updateConnectionListener {
+        void onUpdateConnection();
+        void onBottomReached(int size);
+    }
+
+    private void updateConnection(String user, String Processcase) {
+        ApiInterface apiService = ApiClient.getInstance().getApi();
+        ProgressDialog.getInstance().show(activity);
+        Call<ResponseObj> call = apiService.updateConnections("1069",Processcase, getDeviceId(), qoogol,user);
+        call.enqueue(new Callback<ResponseObj>() {
+            @Override
+            public void onResponse(Call<ResponseObj> call, retrofit2.Response<ResponseObj> response) {
+                try {
+                    ProgressDialog.getInstance().dismiss();
+                    if (response.body()!=null && response.body().getResponse().equalsIgnoreCase("200")){
+                        listener.onUpdateConnection();
+                    } else {
+                        Toast.makeText(activity, UtilHelper.getAPIError(String.valueOf(response.body())),Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ProgressDialog.getInstance().dismiss();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseObj> call, Throwable t) {
+                t.printStackTrace();
+                ProgressDialog.getInstance().dismiss();
+            }
+        });
+    }
+
+
+    @Override
+    public int getItemCount() {
+        return connectionsList.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(@NonNull ConnectionItemBinding itemView) {
+            super(itemView.getRoot());
+
+
+        }
+    }
+}
---
+++ app/src/main/java/com/jangletech/qoogol/ui/connections/BlockedFragment.java
@@ -0,0 +1,172 @@
+package com.jangletech.qoogol.ui.connections;
+
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.widget.SearchView;
+import androidx.databinding.DataBindingUtil;
+import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+import com.jangletech.qoogol.R;
+import com.jangletech.qoogol.adapter.BlockedConnectionAdapter;
+import com.jangletech.qoogol.databinding.FragmentBlockedBinding;
+import com.jangletech.qoogol.dialog.ProgressDialog;
+import com.jangletech.qoogol.model.ConnectionResponse;
+import com.jangletech.qoogol.model.Connections;
+import com.jangletech.qoogol.model.ResponseObj;
+import com.jangletech.qoogol.retrofit.ApiClient;
+import com.jangletech.qoogol.retrofit.ApiInterface;
+import com.jangletech.qoogol.ui.BaseFragment;
+import com.jangletech.qoogol.util.UtilHelper;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+
+import static com.jangletech.qoogol.util.Constant.block;
+import static com.jangletech.qoogol.util.Constant.qoogol;
+import static com.jangletech.qoogol.util.Constant.unblock;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class BlockedFragment extends BaseFragment implements BlockedConnectionAdapter.BlockedItemClick {
+
+    FragmentBlockedBinding mBinding;
+    List<Connections> connectionsList = new ArrayList<>();;
+    private static final String TAG = "FriendsFragment";
+    ApiInterface apiService = ApiClient.getInstance().getApi();
+    BlockedConnectionAdapter mAdapter;
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        mBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_blocked, container, false);
+        setHasOptionsMenu(true);
+        return  mBinding.getRoot();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        getData(0);
+
+    }
+
+    private void getData(int pagestart) {
+        ProgressDialog.getInstance().show(getActivity());
+        Call<ConnectionResponse> call = apiService.fetchConnections("1069",block, getDeviceId(), qoogol,pagestart);
+        call.enqueue(new Callback<ConnectionResponse>() {
+            @Override
+            public void onResponse(Call<ConnectionResponse> call, retrofit2.Response<ConnectionResponse> response) {
+                try {
+                    ProgressDialog.getInstance().dismiss();
+                    connectionsList.clear();
+                    if (response.body()!=null && response.body().getResponse().equalsIgnoreCase("200")){
+                        connectionsList = response.body().getConnection_list();
+                        initView();
+                    } else {
+                        Toast.makeText(getActivity(), UtilHelper.getAPIError(String.valueOf(response.body())),Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ProgressDialog.getInstance().dismiss();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ConnectionResponse> call, Throwable t) {
+                t.printStackTrace();
+                ProgressDialog.getInstance().dismiss();
+            }
+        });
+    }
+    private void initView() {
+        mAdapter = new BlockedConnectionAdapter(getActivity(), connectionsList, this);
+        mBinding.blockedRecycler.setHasFixedSize(true);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        mBinding.blockedRecycler.setLayoutManager(linearLayoutManager);
+        mBinding.blockedRecycler.setAdapter(mAdapter);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+        inflater.inflate(R.menu.forward_action_menu, menu);
+        final MenuItem searchItem = menu.findItem(R.id.action_search);
+        menu.findItem(R.id.action_save).setVisible(false);
+
+        SearchView searchView = (SearchView) searchItem.getActionView();
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                try {
+                    // filter recycler view when query submitted
+                    mAdapter.getFilter().filter(query);
+                } catch (Exception ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                try {
+                    // filter recycler view when text is changed
+                    mAdapter.getFilter().filter(query);
+                } catch (Exception ex) {
+                    Log.e(TAG, ex.getMessage());
+                }
+                return false;
+            }
+        });
+    }
+
+    private void updateConnection(String user, String Processcase) {
+        ApiInterface apiService = ApiClient.getInstance().getApi();
+        ProgressDialog.getInstance().show(getActivity());
+        Call<ResponseObj> call = apiService.updateConnections("1069",Processcase, getDeviceId(), qoogol,user);
+        call.enqueue(new Callback<ResponseObj>() {
+            @Override
+            public void onResponse(Call<ResponseObj> call, retrofit2.Response<ResponseObj> response) {
+                try {
+                    ProgressDialog.getInstance().dismiss();
+                    if (response.body()!=null && response.body().getResponse().equalsIgnoreCase("200")){
+                        getData(0);
+                    } else {
+                        Toast.makeText(getActivity(), UtilHelper.getAPIError(String.valueOf(response.body())),Toast.LENGTH_SHORT).show();
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    ProgressDialog.getInstance().dismiss();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<ResponseObj> call, Throwable t) {
+                t.printStackTrace();
+                ProgressDialog.getInstance().dismiss();
+            }
+        });
+    }
+
+    @Override
+    public void unblockUser(String userId) {
+        updateConnection(userId, unblock);
+    }
+}
