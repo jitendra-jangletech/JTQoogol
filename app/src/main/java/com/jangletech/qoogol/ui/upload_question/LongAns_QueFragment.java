ea9439d0b2c00050f68ef47e78d6d9c2af818b7a
Pritali <pritali.kuyate@jangletech.com>
2020-09-25 16:09:41 +0530

    Merge branch 'Jitendra' of https://github.com/jitendra-jangletech/JTQoogol into Jitendra
    # Conflicts:
    #	.idea/gradle.xml

---
+++ app/src/main/java/com/jangletech/qoogol/ui/upload_question/LongAns_QueFragment.java
@@ -0,0 +1,32 @@
+package com.jangletech.qoogol.ui.upload_question;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jangletech.qoogol.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class LongAns_QueFragment extends Fragment {
+
+
+    public LongAns_QueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_long_ans__que, container, false);
+    }
+
+}
---
+++ app/src/main/java/com/jangletech/qoogol/ui/upload_question/ShortAns_QueFragment.java
@@ -0,0 +1,32 @@
+package com.jangletech.qoogol.ui.upload_question;
+
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.jangletech.qoogol.R;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class ShortAns_QueFragment extends Fragment {
+
+
+    public ShortAns_QueFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_short_ans__que, container, false);
+    }
+
+}
